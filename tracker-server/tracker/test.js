c8d4b53809e0468f9db23afba762f001763f1805
GabrielRicam <gbrielwiranata@gmail.com>
2025-12-26 22:10:13 +0700

    Data update

--- tracker-server/tracker/test.js
+++ tracker-server/tracker/test.js
@@ -1,32 +1,11 @@
-import {resolve, join} from 'path';
 import { readFileSync } from 'fs';
-const img = resolve("./data/media");
-import { format } from 'date-and-time';
-const mangaData = JSON.parse(readFileSync('./../../data/data.json', 'utf8'));
 
-// console.log('\x1b[31mIni teks merah\x1b[0m');
-// console.log('\x1b[32mIni teks hijau\x1b[0m');
-// console.log('\x1b[33mIni teks kuning\x1b[0m');
-// console.log('\x1b[34mIni teks biru\x1b[0m');
-// console.log(`\x1b[32mSuccessfully save s update log\x1b[0m`)
-// process.stdout.write("Noise");
-
-
-// setTimeout(() => console.log("\rHalo dunia"), 1000);
-const a = {
-  "Drawing": {
-    "Latest": [1,2,3],
-    "Time": "2025/11/26 20:53:09",
-    "Status": "Active"
-  },
-  "Bijo to Kenja": {
-    "Latest": [],
-    "Time": "2025/11/26 20:53:18",
-    "Status": "Active"
-  }
+const data = JSON.parse(readFileSync("./../../data/data.json", 'utf-8'));
+const res = [];
+for (let manga of data) {
+  const list = manga[Object.keys(manga)]["Chapter List"];
+  const comp = { [Object.keys(manga)] : list.at(-1) || 0 }
+  res.push(comp)
 }
 
-for (let x of Object.keys(a)) {
-        console.log(a[x]["Latest"]);
-        
-}
+console.log(res)
